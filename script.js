191ee2d51d14748ffa44477e29ddfcfcde3d1f22
rylankearney <rylan.isabel@live.ca>
2022-11-25 15:08:39 -0500

    carousel added for projects section

--- script.js
+++ script.js
@@ -40,3 +40,68 @@ darkModeToggle.addEventListener("click",() => {
 		disableDarkMode();
 	}
 });
+
+
+//CAROUSEL 
+
+const track = document.querySelector('.projects-list');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.right-button');
+const backButton = document.querySelector('.left-button');
+const dotsMenu = document.querySelector('.carousel-tracker');
+const indivDots = Array.from(dotsMenu.children);
+
+
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+//arrange slides next to each other
+const setPosition = (slide, index) => {
+	slide.style.left = slideWidth * (index * 2) + 'px';
+}
+
+slides.forEach(setPosition);
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+	track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+	currentSlide.classList.remove('current');
+	targetSlide.classList.add('current');
+}
+
+//Click left arrow = move slides left
+backButton.addEventListener('click', e => {
+	const currentSlide = track.querySelector('.current');
+	const prevSlide = currentSlide.previousElementSibling;
+
+	moveToSlide(track, currentSlide, prevSlide);
+})
+
+
+//Click right arrow = move slides right
+nextButton.addEventListener('click', e => {
+	const currentSlide = track.querySelector('.current');
+	const nextSlide = currentSlide.nextElementSibling;
+	// const amountMoving = nextSlide.style.left;
+	// //move to the next slide
+	// track.style.transform = 'translateX(-' + amountMoving + ')';
+	// currentSlide.classList.remove('current');
+	// nextSlide.classList.add('current');
+
+	moveToSlide(track, currentSlide, nextSlide);
+})
+
+
+//Click indicators = go to selected slide
+dotsMenu.addEventListener('click', e => {
+	const targetDots = e.target.closest('button');
+
+
+	if(!targetDots) return;
+
+	const currentSlide = track.querySelector('.current');
+	const currentDot = dotsMenu.querySelector('.current');
+	const targetIndex = indivDots.findIndex(dot => dot === targetDots)
+	const targetSlide = slides[targetIndex];
+
+	moveToSlide(track, currentSlide, targetSlide)
+})
